3aae340e8ac2cae078cc733c939958bc5a02cdf1
@LeojAnidem <jeremykaizer@gmail.com>
2023-04-12 17:38:28 -0400

    [<< Recolocacion del transporter de nodemailer >]
    ==========>
    Reubique el transporter del nodemailer en la carpeta 'libs', para
    separar la configuracion del transporter de la funcion de enviar el
    email.

--- src/controllers/recoveryController.js
+++ src/controllers/recoveryController.js
@@ -1,12 +1,4 @@
-import nodemailer from 'nodemailer'
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-})
+import { transporter } from '../libs/transporterConfig.js'
 
 const sendMail = async (from, verificationCode) => {
   const mailOptions = {
